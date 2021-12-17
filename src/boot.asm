cc42b80a9c199289b7cd4c6c2f303b20f3442056
llucere <66846791+llucere@users.noreply.github.com>
2021-12-17 20:33:19 +0200

    b

--- src/boot.asm
+++ src/boot.asm
@@ -1,4 +1,81 @@
+[org 0x7c00]
+jmp start
+
+backspace:
+	mov al, 0x08
+	int 0x10
+	mov al, 0x20
+	int 0x10
+	mov al, 0x08
+	int 0x10
+	ret
+
+pchar:
+	cmp al, 0x08
+	je .isBackspace
+	jmp .else1
+
+	.isBackspace:
+		dec byte[cursor_position]
+		mov ah, 0x0e
+		call backspace
+		jmp .exit
+
+	.else1:
+		cmp al, 0xA
+		je .isEnter
+		cmp al, 0xD
+		je .isEnter
+
+		jmp .else2
+
+		.isEnter:
+		mov bx, new_line
+		call write
+		
+
+		mov bx, cursor_position
+		.lp1:
+			call backspace
+			dec bx
+
+			cmp bx, 0
+			jle .lp1
+
+		mov byte[cursor_position], 0
+		
+		jmp .exit
+
+		.else2:
+		inc byte[cursor_position]
+		mov ah, 0x0e
+		int 0x10
+		jmp .exit
+
+	.exit:
+ret
+
+start:
+
+;mov bx, string001
+;call write
+
+.lp:
+	;mov bx, string002
+	;call write
+
+	mov ah, 0
+	int 0x16
+	call pchar
+jmp .lp
+
 jmp $
 
-times 510-($-$$) db 0
-db 0x55, 0xAA
+cursor_position: db 0
+new_line: db "", 10, 0
+string001: db "Hello, world!", 0
+string002: db "(debug)", 0
+
+%include "inc/sys.inc"
+times 510 - ($ - $$) db 0
+dw 0xAA55
--- src/boot.sh
+++ src/boot.sh
@@ -1,2 +1,5 @@
-nasm -f bin boot.asm -o boot.o
-qemu-system-x86_64 boot.o
+rm -rf build
+mkdir build
+nasm -f bin boot.asm -o build/boot.o
+qemu-system-x86_64 build/boot.o
+rm -rf build
